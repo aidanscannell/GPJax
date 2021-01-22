4a23e39d20f9c7adcfe059eea0970fa33be05e36
Aidan Scannell <scannell.aidan@gmail.com>
2021-01-22 19:49:08 +0000

    Started implementing GPBase class and SVGP

--- gpjax/models/gp.py
+++ gpjax/models/gp.py
@@ -1,126 +1,147 @@
 #!/usr/bin/env python3
+import abc
 from typing import List, Tuple
 
 from gpjax.conditionals import base_conditional
 from gpjax.custom_types import InputData, MeanAndVariance, MeanFunc, OutputData
+from gpjax.kernels import Kernel
 from jax import jacfwd, jacrev, jit
 from jax import numpy as np
 from jax import partial
 from jax import scipy as sp
 from jax import vmap
 
+# create types
+Likelihood = None
+MeanFunction = None
+InducingVariable = None
 
-def gp_predict(
-    Xnew: InputData,
-    X: InputData,
-    kernels,
-    mean_funcs: List[MeanFunc],
-    f: OutputData,
-    full_cov: bool = False,
-    q_sqrt=None,
-    jitter=1e-6,
-    white: bool = True,
-) -> MeanAndVariance:
-    # TODO mapping like this doesn't capture covariance
-    fmeans, fvars = [], []
-    for output_dim, (kernel, mean_func) in enumerate(zip(kernels, mean_funcs)):
-        fmu, fvar = gp_predict_single_output(
-            Xnew,
-            X,
-            kernel,
-            mean_func,
-            f[:, output_dim : output_dim + 1],
-            full_cov=full_cov,
-            q_sqrt=q_sqrt[output_dim : output_dim + 1, :, :],
-            jitter=jitter,
-            white=white,
+
+class GPBase(objax.Module, abc.ABC):
+    def __init__(
+        self,
+        kernel: Kernel,
+        likelihood: Likelihood,
+        mean_function: Optional[MeanFunction] = None,
+        num_latent_gps: int = None,
+    ):
+        assert (
+            num_latent_gps is not None
+        ), "GP requires specification of num_latent_gps"
+        self.num_latent_gps = num_latent_gps
+        self.kernel = Kernel
+        self.likelihood = likelihood
+        if mean_function is None:
+            mean_function = Zero()
+        self.mean_function = mean_function
+
+    @abc.abstractmethod
+    def predict_f(
+        self,
+        Xnew: InputData,
+        full_cov: bool = False,
+        full_output_cov: bool = False,
+    ) -> MeanAndVariance:
+        """ "Compute mean and (co)variance of latent function at Xnew.
+
+        :param Xnew: inputs with shape [num_test, input_dim]
+        :param full_cov:
+            If True, draw correlated samples over Xnew. Computes the Cholesky over the
+            dense covariance matrix of size [num_data, num_data].
+            If False, draw samples that are uncorrelated over the inputs.
+        :param full_output_cov:
+            TODO Not implemented
+        :returns: tuple of Tensors (mean, variance),
+            means.shape == [num_test, output_dim],
+            If full_cov=True and full_output_cov=False,
+                var.shape == [output_dim, num_test, num_test]
+            If full_cov=False,
+                var.shape == [num_test, output_dim]
+        """
+        raise NotImplementedError
+
+    def predict_jacobian_f_at_Xnew(
+        self,
+        Xnew: InputData,
+        full_cov: bool = False,
+        full_output_cov: bool = False,
+    ) -> MeanAndVariance:
+        # TODO implement this method here
+        return NotImplemented
+
+    def predict_y(
+        self,
+        Xnew: InputData,
+        full_cov: bool = False,
+        full_output_cov: bool = False,
+    ) -> MeanAndVariance:
+        """Compute the mean and (co)variance of function at Xnew."""
+        if full_cov or full_output_cov:
+            raise NotImplementedError(
+                "The predict_y method currently supports only the argument values full_cov=False and full_output_cov=False"
+            )
+
+        f_mean, f_var = self.predict_f(
+            Xnew, full_cov=full_cov, full_output_cov=full_output_cov
         )
-        fmeans.append(fmu)
-        fvars.append(fvar)
-    fmeans = np.stack(fmeans)
-    fvars = np.stack(fvars)
-    return fmeans, fvars
+        return self.likelihood.predict_mean_and_var(f_mean, f_var)
 
 
-def gp_predict_single_output(
-    Xnew: InputData,
-    X: InputData,
-    cov_fn,
-    cov_fn_args,
-    mean_func,
-    mean_func_args,
-    f: OutputData,
-    full_cov: bool = False,
-    q_sqrt=None,
-    jitter=1e-6,
-    white: bool = True,
-) -> MeanAndVariance:
-    # TODO add noise???
-    Kmm = Kuu(X, kernel)
-    Kmn = cov_fn(X, X, cov_fn_args, full_cov=True)
+class SVGP(GPBase):
+    def __init__(
+        self,
+        kernel: Kernel,
+        likelihood: Likelihood,
+        inducing_variable: InducingVariable,
+        mean_function: MeanFunction = None,
+        num_latent_gps: int = 1,
+        q_diag: bool = False,
+        q_mu=None,
+        q_sqrt=None,
+        whiten: bool = True,
+        # num_data=None,
+    ):
+        super().__init__(
+            kernel=kernel,
+            likelihood=likelihood,
+            mean_function=mean_function,
+            num_latent_gps=num_latent_gps,
+        )
+        self.whiten = whiten
+        self.q_diag = q_diag
 
-    # Kmm += jitter * np.eye(Kmm.shape[0])
-    Kmn = cov_fn(X, Xnew, cov_fn_args, full_cov=True)
-    Knn = cov_fn(Xnew, Xnew, cov_fn_args, full_cov=full_cov)
+        # TODO set q_mu/q_sqrt if = None
+        self.inducing_variable = inducing_variable
+        self.q_mu = q_mu
+        self.q_sqrt = q_sqrt
 
-    print("q_sqrt")
-    print(q_sqrt.shape)
-    if q_sqrt is not None:
-        # TODO map over output dimension
-        q_sqrt = np.squeeze(q_sqrt)
-        q_sqrt = q_sqrt.reshape([q_sqrt.shape[-1], q_sqrt.shape[-1]])
+    def predict_f(
+        self, Xnew: InputData, full_cov=False, full_output_cov=False
+    ) -> MeanAndVariance:
+        # mean, cov = conditional(
+        #     Xnew,
+        #     self.inducing_variable,
+        #     self.kernel,
+        #     self.q_mu,
+        #     q_sqrt=self.q_sqrt,
+        #     full_cov=full_cov,
+        #     white=self.whiten,
+        #     full_output_cov=full_output_cov,
+        # )
 
-    # TODO map over output dimension of Y??
-    # f += mean_func
-    fmean, fvar = base_conditional(
-        Kmn=Kmn,
-        Kmm=Kmm,
-        Knn=Knn,
-        f=f,
-        full_cov=full_cov,
-        q_sqrt=q_sqrt,
-        white=white,
-    )
-    # return fmean, fvar
-    mean = mean_func(Xnew, mean_func_args)
-    return fmean + mean, fvar
+        # TODO map over output dimension of Y??
+        mean, cov = conditional(
+            Xnew,
+            inducing_variable,
+            kernel=self.kernel,
+            f=self.q_mu,
+            full_cov=self.full_cov,
+            full_output_cov=self.full_output_cov,
+            q_sqrt=self.q_sqrt,
+            white=self.whiten,
+            jitter=self.jitter)
+        return mean + self.mean_function(Xnew), cov
 
 
-# def gp_predict(Xnew: InputData,
-#                X: InputData,
-#                kernel,
-#                mean_func: MeanFunc,
-#                f: OutputData,
-#                *,
-#                full_cov: bool = False,
-#                q_sqrt=None,
-#                jitter=1e-6,
-#                white: bool = True) -> MeanAndVariance:
-#     # TODO add noise???
-#     Kmm = Kuu(X, kernel)
 
-#     # Kmm += jitter * np.eye(Kmm.shape[0])
-#     Kmn = kernel.K(X, Xnew)
-#     if full_cov:
-#         Knn = kernel.K(Xnew, Xnew)
-#     else:
-#         Knn = kernel.Kdiag(Xnew)
 
-#     print('q_sqrt')
-#     print(q_sqrt.shape)
-#     # if q_sqrt is not None:
-#     # TODO map over output dimension
-#     # q_sqrt = np.squeeze(q_sqrt)
-#     # q_sqrt = q_sqrt.reshape([q_sqrt.shape[-1], q_sqrt.shape[-1]])
-
-#     # TODO map over output dimension of Y??
-#     # f += mean_func
-#     fmean, fvar = base_conditional(Kmn=Kmn,
-#                                    Kmm=Kmm,
-#                                    Knn=Knn,
-#                                    f=f,
-#                                    full_cov=full_cov,
-#                                    q_sqrt=q_sqrt,
-#                                    white=white)
-#     # return fmean, fvar
-#     return fmean + mean_func, fvar
