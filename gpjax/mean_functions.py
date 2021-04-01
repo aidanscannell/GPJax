18843512e2508c651bcade7ab66563f954ceff3e
Aidan Scannell <scannell.aidan@gmail.com>
2021-04-01 16:23:12 +0100

    converted mean_functions and test to raw jax
    no objax or haiku

--- gpjax/mean_functions.py
+++ gpjax/mean_functions.py
@@ -1,11 +1,13 @@
 # import jax
+import abc
 import jax.numpy as jnp
-import objax
+import jax
 
 from gpjax.custom_types import InputData, OutputData
+from gpjax.base import Module
 
 
-class MeanFunction(objax.Module):
+class MeanFunction(Module, abc.ABC):
     """The base mean function class.
 
     Each mean function must implement the __call__ method.
@@ -17,69 +19,74 @@ class MeanFunction(objax.Module):
         else:
             self.default_int = default_int
 
-    def __call__(self, X: InputData) -> OutputData:
+    @jax.partial(jax.jit, static_argnums=0)
+    def __call__(self, params: dict, X: InputData) -> OutputData:
         """Return the mean functon evaluated at the input tensor X
 
         This takes a tensor X and returns a tensor m(X).
         Note that MeanFunction classes can have trainable variables.
+        Preserves leading dimensions in X.
 
-        :param X: input tensor [num_test, input_dim]
-        :returns: output tensor m(X) [num_test, output_dim]
+        :param X: input tensor [..., num_test, input_dim]
+        :returns: output tensor m(X) [..., num_test, output_dim]
         """
         raise NotImplementedError(
             "Implement the __call__ method for this mean function"
         )
 
-    def __add__(self, other):
-        return Additive(self, other)
+    # def __add__(self, other):
+    #     return Additive(self, other)
 
-    def __mul__(self, other):
-        return Product(self, other)
-
-
-class Additive(MeanFunction):
-    def __init__(self, first_part, second_part):
-        MeanFunction.__init__(self)
-        self.add_1 = first_part
-        self.add_2 = second_part
-
-    def __call__(self, X):
-        return jnp.add(self.add_1(X), self.add_2(X))
+    # def __mul__(self, other):
+    #     return Product(self, other)
 
 
-class Product(MeanFunction):
-    def __init__(self, first_part, second_part):
-        MeanFunction.__init__(self)
+# class Additive(MeanFunction):
+#     def __init__(self, first_part, second_part):
+#         MeanFunction.__init__(self)
+#         self.add_1 = first_part
+#         self.add_2 = second_part
 
-        self.prod_1 = first_part
-        self.prod_2 = second_part
+#     def __call__(self, X):
+#         return jnp.add(self.add_1(X), self.add_2(X))
 
-    def __call__(self, X):
-        return jnp.multiply(self.prod_1(X), self.prod_2(X))
+
+# class Product(MeanFunction):
+#     def __init__(self, first_part, second_part):
+#         MeanFunction.__init__(self)
+
+#         self.prod_1 = first_part
+#         self.prod_2 = second_part
+
+#     def __call__(self, X):
+#         return jnp.multiply(self.prod_1(X), self.prod_2(X))
 
 
 class Constant(MeanFunction):
-    def __init__(self, c=None):
+    def __init__(self, c=None, output_dim=1):
         super().__init__()
         if c is None:
             c = jnp.zeros(1)
-        # c = jnp.zeros(1) if c is None else c
-        self.const_train_var = objax.TrainVar(c)
-        self.c = objax.TrainRef(self.const_train_var)
+        self.c = c
+        self.output_dim = output_dim
 
-    def __call__(self, X):
-        tile_shape = jnp.concatenate(
-            [jnp.array(jnp.shape(X)[:-1]), jnp.array([1])],
-            axis=0,
-        )
-        reshape_shape = jnp.concatenate(
-            [
-                jnp.ones(shape=(X.ndim - 1), dtype=self.default_int),
-                jnp.array([-1]),
-            ],
-            axis=0,
-        )
-        return jnp.tile(jnp.reshape(self.c.value, reshape_shape), tile_shape)
+    def init_params(
+        self,
+    ) -> dict:
+        return {"constant": self.c}
+
+    @jax.partial(jax.jit, static_argnums=0)
+    def __call__(self, params: dict, X: InputData) -> OutputData:
+        """Returns constant with shape (X.shape[:-1], output_dim)
+
+        Preserves leading dimensions in X.
+
+        :param X: input tensor [..., num_test, input_dim]
+        :returns: output tensor m(X) [..., num_test, output_dim]
+        """
+        c = params["constant"]
+        output_shape = (*jnp.shape(X)[:-1], self.output_dim)
+        return jnp.ones(output_shape, dtype=X.dtype) * params["constant"]
 
 
 class Zero(Constant):
@@ -88,9 +95,110 @@ class Zero(Constant):
         self.output_dim = output_dim
         del self.c
 
-    def __call__(self, X):
-        output_shape = jnp.concatenate(
-            [jnp.array(jnp.shape(X)[:-1]), jnp.array([self.output_dim])],
-            axis=0,
-        )
+    def init_params(
+        self,
+    ) -> dict:
+        return None
+
+    @jax.partial(jax.jit, static_argnums=(0))
+    def __call__(self, params: dict, X: InputData) -> OutputData:
+        """Returns zeros with shape the input tensor (X.shape[:-1], output_dim)
+
+        Preserves leading dimensions in X.
+
+        :param X: input tensor [..., num_test, input_dim]
+        :returns: output tensor m(X) [..., num_test, output_dim]
+        """
+        output_shape = (*jnp.shape(X)[:-1], self.output_dim)
         return jnp.zeros(output_shape, dtype=X.dtype)
+
+
+# class MeanFunction(objax.Module):
+#     """The base mean function class.
+
+#     Each mean function must implement the __call__ method.
+#     """
+
+#     def __init__(self, default_int=None):
+#         if default_int is None:
+#             self.default_int = jnp.int64
+#         else:
+#             self.default_int = default_int
+
+#     def __call__(self, X: InputData) -> OutputData:
+#         """Return the mean functon evaluated at the input tensor X
+
+#         This takes a tensor X and returns a tensor m(X).
+#         Note that MeanFunction classes can have trainable variables.
+
+#         :param X: input tensor [num_test, input_dim]
+#         :returns: output tensor m(X) [num_test, output_dim]
+#         """
+#         raise NotImplementedError(
+#             "Implement the __call__ method for this mean function"
+#         )
+
+#     def __add__(self, other):
+#         return Additive(self, other)
+
+#     def __mul__(self, other):
+#         return Product(self, other)
+
+
+# class Additive(MeanFunction):
+#     def __init__(self, first_part, second_part):
+#         MeanFunction.__init__(self)
+#         self.add_1 = first_part
+#         self.add_2 = second_part
+
+#     def __call__(self, X):
+#         return jnp.add(self.add_1(X), self.add_2(X))
+
+
+# class Product(MeanFunction):
+#     def __init__(self, first_part, second_part):
+#         MeanFunction.__init__(self)
+
+#         self.prod_1 = first_part
+#         self.prod_2 = second_part
+
+#     def __call__(self, X):
+#         return jnp.multiply(self.prod_1(X), self.prod_2(X))
+
+
+# class Constant(MeanFunction):
+#     def __init__(self, c=None):
+#         super().__init__()
+#         if c is None:
+#             c = jnp.zeros(1)
+#         # c = jnp.zeros(1) if c is None else c
+#         self.const_train_var = objax.TrainVar(c)
+#         self.c = objax.TrainRef(self.const_train_var)
+
+#     def __call__(self, X):
+#         tile_shape = jnp.concatenate(
+#             [jnp.array(jnp.shape(X)[:-1]), jnp.array([1])],
+#             axis=0,
+#         )
+#         reshape_shape = jnp.concatenate(
+#             [
+#                 jnp.ones(shape=(X.ndim - 1), dtype=self.default_int),
+#                 jnp.array([-1]),
+#             ],
+#             axis=0,
+#         )
+#         return jnp.tile(jnp.reshape(self.c.value, reshape_shape), tile_shape)
+
+
+# class Zero(Constant):
+#     def __init__(self, output_dim=1):
+#         Constant.__init__(self)
+#         self.output_dim = output_dim
+#         del self.c
+
+#     def __call__(self, X):
+#         output_shape = jnp.concatenate(
+#             [jnp.array(jnp.shape(X)[:-1]), jnp.array([self.output_dim])],
+#             axis=0,
+#         )
+#         return jnp.zeros(output_shape, dtype=X.dtype)
--- tests/test_mean_functions.py
+++ tests/test_mean_functions.py
@@ -1,73 +1,127 @@
+import chex
 import jax
-import jax.numpy as jnp
 import pytest
-# from gpjax.mean_functions import Constant, Linear, Zero
-from gpjax.mean_functions import Additive, Constant, Product, Zero
+from absl.testing import parameterized
+from gpjax.utilities.ops import leading_transpose
+from jax import numpy as jnp
+
+from gpjax.mean_functions import Zero, Constant
 
 jax.config.update("jax_enable_x64", True)
-key = jax.random.PRNGKey(0)
 
 
-class Datum:
-    input_dim, output_dim = 3, 2
-    N, Ntest, M = 20, 30, 10
+@chex.dataclass
+class TestData:
+    X: chex.ArrayDevice
+    Y: chex.ArrayDevice
 
 
-# Constant(c=jax.random.normal(key, shape=(Datum.output_dim,))),
+data = TestData(
+    X=jnp.linspace(0, 10, 20).reshape(10, 2), Y=jnp.linspace(0, 10, 30).reshape(30, 1)
+)
+batched_data = TestData(
+    X=jnp.linspace(0, 10, 80 * 5).reshape(5, 4, 10, 2),
+    Y=jnp.linspace(0, 10, 80 * 5).reshape(5, 4, 10, 2),
+    # X2=jnp.linspace(0, 10, 240).reshape(5, 4, 6, 2),
+)
 
 
-class Data:
-    x1 = jnp.linspace(0, 10, 20).reshape(10, 2)
+class TestMeanFunctions(chex.TestCase):
+    # @chex.assert_max_traces(n=1)
+    @chex.variants(with_jit=True, without_jit=True)
+    @parameterized.product(X=[data.X, batched_data.X], output_dim=[1, 2, 5])
+    def test_Zero(self, X, output_dim):
+        mean_function = Zero(output_dim=output_dim)
+        params = mean_function.init_params()
+        var_mean_function = self.variant(mean_function)
+
+        mean = var_mean_function(params, X)
+        assert mean.shape[-1] == output_dim
+        assert mean.shape[:-1] == X.shape[:-1]
+
+    @chex.variants(with_jit=True, without_jit=True)
+    @parameterized.product(X=[data.X, batched_data.X], output_dim=[1, 2, 5])
+    def test_Constant(self, X, output_dim):
+        constant = 4.0
+        mean_function = Constant(c=constant, output_dim=output_dim)
+        params = mean_function.init_params()
+        var_mean_function = self.variant(mean_function)
+
+        mean = var_mean_function(params, X)
+        assert mean.shape[-1] == output_dim
+        assert mean.shape[:-1] == X.shape[:-1]
 
 
-_mean_functions = [
-    Zero(),
-    # Linear(
-    #     A=rng.randn(Datum.input_dim, Datum.output_dim),
-    #     b=rng.randn(Datum.output_dim, 1).reshape(-1),
-    # ),
-    Constant(c=jax.random.normal(key, shape=(Datum.output_dim,))),
-]
+# import jax
+# import jax.numpy as jnp
+# import pytest
+# # from gpjax.mean_functions import Constant, Linear, Zero
+# from gpjax.mean_functions import Additive, Constant, Product, Zero
+
+# jax.config.update("jax_enable_x64", True)
+# key = jax.random.PRNGKey(0)
 
 
-@pytest.mark.parametrize("mean_function_1", _mean_functions)
-@pytest.mark.parametrize("mean_function_2", _mean_functions)
-@pytest.mark.parametrize("operation", ["+", "*"])
-def test_mean_functions_output_shape(
-    mean_function_1, mean_function_2, operation
-):
-    """
-    Test the output shape for basic and compositional mean functions, also
-    check that the combination of mean functions returns the correct class
-    """
-    X = jax.random.normal(key, shape=(Datum.N, Datum.input_dim))
-    Y = mean_function_1(X)
-    # basic output shape check
-    assert Y.shape in [(Datum.N, Datum.output_dim), (Datum.N, 1)]
-
-    # composed mean function output shape check
-    if operation == "+":
-        mean_composed = mean_function_1 + mean_function_2
-    elif operation == "*":
-        mean_composed = mean_function_1 * mean_function_2
-    else:
-        raise (NotImplementedError)
-
-    Y_composed = mean_composed(X)
-    assert Y_composed.shape in [(Datum.N, Datum.output_dim), (Datum.N, 1)]
+# class Datum:
+#     input_dim, output_dim = 3, 2
+#     N, Ntest, M = 20, 30, 10
 
 
-@pytest.mark.parametrize("mean_function_1", _mean_functions)
-@pytest.mark.parametrize("mean_function_2", _mean_functions)
-@pytest.mark.parametrize("operation", ["+", "*"])
-def test_mean_functions_composite_type(
-    mean_function_1, mean_function_2, operation
-):
-    if operation == "+":
-        mean_composed = mean_function_1 + mean_function_2
-        assert isinstance(mean_composed, Additive)
-    elif operation == "*":
-        mean_composed = mean_function_1 * mean_function_2
-        assert isinstance(mean_composed, Product)
-    else:
-        raise (NotImplementedError)
+# # Constant(c=jax.random.normal(key, shape=(Datum.output_dim,))),
+
+
+# class Data:
+#     x1 = jnp.linspace(0, 10, 20).reshape(10, 2)
+
+
+# _mean_functions = [
+#     Zero(),
+#     # Linear(
+#     #     A=rng.randn(Datum.input_dim, Datum.output_dim),
+#     #     b=rng.randn(Datum.output_dim, 1).reshape(-1),
+#     # ),
+#     Constant(c=jax.random.normal(key, shape=(Datum.output_dim,))),
+# ]
+
+
+# @pytest.mark.parametrize("mean_function_1", _mean_functions)
+# @pytest.mark.parametrize("mean_function_2", _mean_functions)
+# @pytest.mark.parametrize("operation", ["+", "*"])
+# def test_mean_functions_output_shape(
+#     mean_function_1, mean_function_2, operation
+# ):
+#     """
+#     Test the output shape for basic and compositional mean functions, also
+#     check that the combination of mean functions returns the correct class
+#     """
+#     X = jax.random.normal(key, shape=(Datum.N, Datum.input_dim))
+#     Y = mean_function_1(X)
+#     # basic output shape check
+#     assert Y.shape in [(Datum.N, Datum.output_dim), (Datum.N, 1)]
+
+#     # composed mean function output shape check
+#     if operation == "+":
+#         mean_composed = mean_function_1 + mean_function_2
+#     elif operation == "*":
+#         mean_composed = mean_function_1 * mean_function_2
+#     else:
+#         raise (NotImplementedError)
+
+#     Y_composed = mean_composed(X)
+#     assert Y_composed.shape in [(Datum.N, Datum.output_dim), (Datum.N, 1)]
+
+
+# @pytest.mark.parametrize("mean_function_1", _mean_functions)
+# @pytest.mark.parametrize("mean_function_2", _mean_functions)
+# @pytest.mark.parametrize("operation", ["+", "*"])
+# def test_mean_functions_composite_type(
+#     mean_function_1, mean_function_2, operation
+# ):
+#     if operation == "+":
+#         mean_composed = mean_function_1 + mean_function_2
+#         assert isinstance(mean_composed, Additive)
+#     elif operation == "*":
+#         mean_composed = mean_function_1 * mean_function_2
+#         assert isinstance(mean_composed, Product)
+#     else:
+#         raise (NotImplementedError)
