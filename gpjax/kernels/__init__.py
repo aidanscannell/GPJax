e10c52ea049f71efe1ed9592f37fd970532f81c8
Aidan Scannell <scannell.aidan@gmail.com>
2021-04-07 13:07:39 +0100

    Updated __init__ to import Kenrels and functions

--- gpjax/kernels/__init__.py
+++ gpjax/kernels/__init__.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # from gpjax.kernels.base import Kernel
-from gpjax.kernels.kernels import (
-    Kernel,
-    # Cosine,
+from gpjax.kernels.base import Kernel, Combination, covariance_map
+from gpjax.kernels.stationaries import (
+    squared_exponential_cov_fn,
     SquaredExponential,
     Stationary,
 )
 
 RBF = SquaredExponential
+
+from gpjax.kernels.multioutput import SeparateIndependent
