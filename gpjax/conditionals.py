9c24946de95e1601285f08e239b857722c664eaa
Aidan Scannell <scannell.aidan@gmail.com>
2021-04-13 15:34:51 +0100

    Fixed single_output_conditional for Kernel
    Corrected typing arrays for conditional().
    Deleted unused functions.

--- gpjax/conditionals.py
+++ gpjax/conditionals.py
@@ -25,13 +25,16 @@ def conditional(
     Xnew: tjax.Array2[NumData, InputDim],
     X: tjax.Array2[NumInducing, InputDim],
     # inducing_variable: InducingVariable,
+    # TODO implement dispatching for inducing variables
     kernel: Kernel,
-    f: tjax.Array1[NumInducing],
+    f: Union[tjax.Array2[NumInducing, OutputDim], tjax.Array1[NumInducing]],
     full_cov: Optional[bool] = False,
     full_output_cov: Optional[bool] = False,
     q_sqrt: Optional[
         Union[
+            tjax.Array3[OutputDim, NumInducing, NumInducing],
             tjax.Array2[NumInducing, NumInducing],
+            tjax.Array2[NumInducing, OutputDim],
             tjax.Array1[NumInducing],
         ]
     ] = None,
@@ -44,7 +47,8 @@ def conditional(
     f_mean, f_cov = single_output_conditional(
         kernel_params,
         Xnew,
-        inducing_variable,
+        # inducing_variable,
+        X,
         kernel,
         f=f,
         full_cov=full_cov,
@@ -79,14 +83,36 @@ def single_output_conditional(
     Kmn = kernel(kernel_params, X, Xnew)  # [M, N]
     Knn = kernel(kernel_params, Xnew, full_cov=full_cov)  # [N, N]
 
-    return base_conditional(
-        Kmn, Kmm, Knn, f, full_cov=full_cov, q_sqrt=q_sqrt, white=white
-    )  # [N]
+    def base_conditional_wrapper(f_, q_sqrt_):
+        return base_conditional(
+            Kmn, Kmm, Knn, f_, full_cov=full_cov, q_sqrt=q_sqrt_, white=white
+        )
+
+    # setup axis containing output dim which are to be mapped over
+    if full_cov:
+        # [output_dim, num_data, num_data]
+        out_axes = (-1, 0)
+    else:
+        # [num_data, output_dim]
+        out_axes = (-1, -1)
+    if q_sqrt is not None:
+        if q_sqrt.ndim == 2:
+            in_axes = (-1, -1)
+        elif q_sqrt.ndim == 3:
+            in_axes = (-1, 0)
+        else:
+            raise ValueError("Bad dimension for q_sqrt: %s" % str(q_sqrt.ndim))
+
+    f_mean, f_cov = jax.vmap(
+        base_conditional_wrapper, in_axes=in_axes, out_axes=out_axes
+    )(f, q_sqrt)
+    return f_mean, f_cov
 
 
 # @conditional.dispatch(None, None, None, MultioutputKernel)
 @conditional.dispatch(None, None, None, SeparateIndependent)
 def independent_output_conditional(
+    # def conditional(
     kernel_params: dict,
     Xnew: tjax.Array2[NumData, InputDim],
     X: tjax.Array2[NumInducing, InputDim],
@@ -162,74 +188,6 @@ def fully_correlated_conditional(
 ):
     """Multi-output GP conditional where the conditioning points are fully correlated."""
     raise NotImplementedError("Still needs to be implemented")
-
-
-# def independent_output_conditional(
-#     # def conditional(
-#     kernel_params: dict,
-#     Xnew: tjax.Array2[NumData, InputDim],
-#     inducing_variable: InducingVariable,
-#     kernel: Union[Kernel, MultioutputKernel],
-#     f: tjax.Array2[NumInducing, OutputDim],
-#     full_cov: Optional[bool] = False,
-#     full_output_cov: Optional[bool] = False,
-#     q_sqrt: Optional[
-#         Union[
-#             tjax.Array3[OutputDim, NumInducing, NumInducing],
-#             tjax.Array2[NumInducing, OutputDim],
-#         ]
-#     ] = None,
-#     white: Optional[bool] = False,
-# ):
-#     """Multi-output GP conditional where outputs are assumed independent."""
-#     # TODO map over kernels
-#     print("inside multi output gp conditional")
-#     print(Xnew.shape)
-#     print(inducing_variable.shape)
-#     print(f.shape)
-#     print(q_sqrt.shape)
-
-#     def single_output_conditional_wrapper(f_, q_sqrt_=None):
-#         print("vmap inside yo")
-#         print(f_.shape)
-#         print(q_sqrt_.shape)
-#         f_mean, f_cov = single_output_conditional(
-#             # kernel_params,
-#             kernel_params[0],
-#             Xnew,
-#             inducing_variable,
-#             kernel.kernels[0],
-#             # kernel,
-#             f_,
-#             full_cov=full_cov,
-#             q_sqrt=q_sqrt_,
-#             white=white,
-#         )
-#         return f_mean, f_cov
-
-#     # output should always be last dimension for mean
-#     # but changes for covariance depending on full_cov
-#     if full_cov:
-#         # [output_dim, num_data, num_data]
-#         out_axes = (-1, 0)
-#     else:
-#         # [num_data, output_dim]
-#         out_axes = (-1, -1)
-#     if q_sqrt is not None:
-#         if q_sqrt.ndim == 2:
-#             in_axes = (-1, -1)
-#         elif q_sqrt.ndim == 3:
-#             in_axes = (-1, 0)
-#         else:
-#             raise ValueError("Bad dimension for q_sqrt: %s" % str(q_sqrt.ndim))
-#         F_mean, F_cov = jax.vmap(
-#             single_output_conditional_wrapper, in_axes=in_axes, out_axes=out_axes
-#         )(f, q_sqrt)
-#     else:
-#         F_mean, F_cov = jax.vmap(
-#             single_output_conditional_wrapper, in_axes=(-1), out_axes=out_axes
-#         )(f)
-#     return F_mean, F_cov
 
 
 def base_conditional(
@@ -321,114 +279,3 @@ def base_conditional_with_lm(
             fvar = fvar + jnp.sum(jnp.square(LTA), -2)  # [N]
 
     return fmean, fvar
-
-
-# def independent_output_conditional(
-# def conditional(
-#     kernel_params: dict,
-#     Xnew: jnp.DeviceArray,
-#     inducing_variable: InducingVariable,
-#     kernel: Union[Kernel, MultioutputKernel],
-#     f: jnp.DeviceArray,
-#     full_cov: bool = False,
-#     full_output_cov: bool = False,
-#     q_sqrt: jnp.DeviceArray = None,
-#     white: bool = False,
-#     jitter: jnp.float64 = 1e-4,
-# ):
-#     """Multi-output GP conditional where outputs are assumed independent."""
-#     print("inside multi output gp conditional")
-#     print(Xnew.shape)
-#     print(inducing_variable.shape)
-#     print(f.shape)
-#     print(q_sqrt.shape)
-
-#     def single_output_conditional_wrapper(f_, q_sqrt_=None):
-#         print("vmap inside yo")
-#         print(f_.shape)
-#         print(q_sqrt_.shape)
-#         f_mean, f_cov = single_output_conditional(
-#             # kernel_params,
-#             kernel_params[0],
-#             Xnew,
-#             inducing_variable,
-#             kernel.kernels[0],
-#             # kernel,
-#             f_,
-#             full_cov=full_cov,
-#             q_sqrt=q_sqrt_,
-#             white=white,
-#         )
-#         return f_mean, f_cov
-
-#     if q_sqrt is not None:
-#         if q_sqrt.ndim == 3:
-#             F_mean, F_cov = jax.vmap(
-#                 single_output_conditional_wrapper, in_axes=(-1, 0)
-#             )(f, q_sqrt)
-#         else:
-#             raise NotImplementedError(
-#                 "Need to implement conditionals for diagonal q_sqrt!"
-#             )
-#     else:
-#         F_mean, F_cov = jax.vmap(single_output_conditional_wrapper, in_axes=(-1))(f)
-#     return F_mean, F_cov
-
-
-# def conditional_(
-#     kernel_params: dict,
-#     Xnew: tjax.Array2[NumData, InputDim],
-#     inducing_variable: InducingVariable,
-#     kernel: Kernel,
-#     f: tjax.Array2[NumInducing, OutputDim],
-#     full_cov: Optional[bool] = False,
-#     full_output_cov: Optional[bool] = False,
-#     q_sqrt: Optional[
-#         Union[
-#             tjax.Array3[OutputDim, NumInducing, NumInducing],
-#             tjax.Array2[NumInducing, OutputDim],
-#         ]
-#     ] = None,
-#     white: Optional[bool] = False,
-# ):
-#     """Single-output GP conditional."""
-#     print("inside conditional")
-#     print(Xnew.shape)
-#     print(inducing_variable.shape)
-#     print(f.shape)
-#     print(q_sqrt.shape)
-#     if isinstance(kernel, MultioutputKernel):
-#         kernel = kernel.kernels[0]
-#         kernel_params = kernel_params[0]
-
-#         # TODO jitter should be noise (sampled)
-#         Kmm = kernel(kernel_params, inducing_variable, inducing_variable)
-#         print("Kmm")
-#         print(Kmm.shape)
-#         Kmn = kernel(kernel_params, inducing_variable, Xnew)
-#         print(Kmn.shape)
-#         Knn = kernel(kernel_params, Xnew, full_cov=False)
-#         print(Knn.shape)
-#         # if full_cov:
-#         #     Knn = kernel(params["kernel"], Xnew)
-#         #     # Knn += jitter * jnp.eye(Knn.shape[0], dtype=default_float())
-#         # else:
-#         #     Knn = kernel.K_diag(params["kernel"], Xnew)
-#         #     # Knn = kernel.K_diag(Xnew) + jitter
-
-#         # # TODO handle multioutput GPs
-#         return base_conditional(
-#             Kmn,
-#             Kmm,
-#             Knn,
-#             f[:, 0],
-#             full_cov=full_cov,
-#             q_sqrt=q_sqrt[0, :, :],
-#             white=white,
-#         )
-#     # if len(q_sqrt.shape) == 3 and q_sqrt.shape[0] == 1:
-#     #     return base_conditional(
-#     #         Kmn, Kmm, Knn, f, full_cov=full_cov, q_sqrt=q_sqrt, white=white
-#     #     )
-#     # else:
-#     #     raise ValueError("Multioutput GPs not implemented yet")
