e589d17021ae57c93686450d319f1e92c0107448
Aidan Scannell <scannell.aidan@gmail.com>
2021-04-30 13:02:45 +0100

    added bernoulli logdensity

--- gpjax/logdensities.py
+++ gpjax/logdensities.py
@@ -4,3 +4,11 @@ import jax.numpy as jnp
 
 def gaussian(x, mu, var):
     return -0.5 * (jnp.log(2 * jnp.pi) + jnp.log(var) + jnp.square(mu - x) / var)
+
+
+def bernoulli(x, p):
+    pred = x == 1
+    print("pred")
+    print(pred)
+    return jnp.log(jnp.where(pred, p, 1 - p))
+    # return jnp.log(tf.where(tf.equal(x, 1), p, 1 - p))
