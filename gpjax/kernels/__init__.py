4f0dd77f15828e0d0f7c5abbf52ec35fbf49512e
Aidan Scannell <scannell.aidan@gmail.com>
2021-05-10 16:38:28 +0100

    changed kernel_decorator to covariance_decorator

--- gpjax/kernels/__init__.py
+++ gpjax/kernels/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from gpjax.kernels.base import Kernel, Combination, kernel_decorator
+from gpjax.kernels.base import Kernel, Combination, covariance_decorator
 from gpjax.kernels.stationaries import (
     squared_exponential_cov_fn,
     SquaredExponential,
     Stationary,
+    Rectangle,
 )
 
 RBF = SquaredExponential
